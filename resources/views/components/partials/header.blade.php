0f4a1733250eafcd2263343a4689133d391a6dd4
praga arya <aryapragar@gmail.com>
2026-03-14 13:55:22 +0700

    memberikan kondisional btn tambahan

--- resources/views/components/partials/header.blade.php
+++ resources/views/components/partials/header.blade.php
@@ -8,72 +8,84 @@
       @isset($permission)
       @can($permission)
       <div class="col-auto ms-auto d-print-none">
-        <button type="button"
-          wire:click="create"
-          wire:loading.attr="disabled"
-          wire:target="create"
-          class="btn btn-primary btn-sm-custom">
-
-          <svg 
-            wire:loading.remove 
-            wire:target="create" 
-            xmlns="http://www.w3.org/2000/svg" 
-            class="icon m-0" 
-            width="24" height="24" 
-            viewBox="0 0 24 24" 
-            stroke-width="2" 
-            stroke="currentColor" 
-            fill="none" 
-            stroke-linecap="round" 
-            stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 5l0 14" />
-            <path d="M5 12l14 0" />
-          </svg>
-
-          <span
-            wire:loading
+        <div class="btn-group" role="group" style="gap: 6px;">
+          <button type="button"
+            wire:click="create"
+            wire:loading.attr="disabled"
             wire:target="create"
-            class="spinner-border spinner-custom">
-          </span>
-          
-          <span class="d-none d-sm-inline ms-1">Tambah</span>
-        </button>
+            class="btn btn-primary btn-sm-custom">
+
+            <svg 
+              wire:loading.remove 
+              wire:target="create" 
+              xmlns="http://www.w3.org/2000/svg" 
+              class="icon m-0" 
+              width="24" height="24" 
+              viewBox="0 0 24 24" 
+              stroke-width="2" 
+              stroke="currentColor" 
+              fill="none" 
+              stroke-linecap="round" 
+              stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 5l0 14" />
+              <path d="M5 12l14 0" />
+            </svg>
+
+            <span
+              wire:loading
+              wire:target="create"
+              class="spinner-border spinner-custom">
+            </span>
+            
+            <span class="d-none d-sm-inline ms-1">Tambah</span>
+          </button>
+
+          @isset($extraButtons)
+          {!! $extraButtons !!}
+          @endisset
+        </div>
       </div>
       @endcan
       @else
       <div class="col-auto ms-auto d-print-none">
-        <button type="button"
-          wire:click="create"
-          wire:loading.attr="disabled"
-          wire:target="create"
-          class="btn btn-primary btn-sm-custom">
-
-          <svg 
-            wire:loading.remove 
-            wire:target="create" 
-            xmlns="http://www.w3.org/2000/svg" 
-            class="icon m-0" 
-            width="24" height="24" 
-            viewBox="0 0 24 24" 
-            stroke-width="2" 
-            stroke="currentColor" 
-            fill="none" 
-            stroke-linecap="round" 
-            stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 5l0 14" />
-            <path d="M5 12l14 0" />
-          </svg>
-
-          <span
-            wire:loading
+        <div class="btn-group" role="group" style="gap: 6px;">
+          <button type="button"
+            wire:click="create"
+            wire:loading.attr="disabled"
             wire:target="create"
-            class="spinner-border spinner-custom">
-          </span>
-          
-          <span class="d-none d-sm-inline ms-1">Tambah</span>
-        </button>
+            class="btn btn-primary btn-sm-custom">
+
+            <svg 
+              wire:loading.remove 
+              wire:target="create" 
+              xmlns="http://www.w3.org/2000/svg" 
+              class="icon m-0" 
+              width="24" height="24" 
+              viewBox="0 0 24 24" 
+              stroke-width="2" 
+              stroke="currentColor" 
+              fill="none" 
+              stroke-linecap="round" 
+              stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M12 5l0 14" />
+              <path d="M5 12l14 0" />
+            </svg>
+
+            <span
+              wire:loading
+              wire:target="create"
+              class="spinner-border spinner-custom">
+            </span>
+            
+            <span class="d-none d-sm-inline ms-1">Tambah</span>
+          </button>
+
+          @isset($extraButtons)
+          {!! $extraButtons !!}
+          @endisset
+        </div>
       </div>
       @endisset
 
